0ecaf6bb5fed9f2df5ad7633dbbeba5f1a264e8c
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2022-06-04 20:32:01 +0300

    add lesson 15 - front

---
+++ 15_frontend/README.md
@@ -0,0 +1,23 @@
+```
+npx hardhat clean
+npx hardhat node
+npx hardhat run scripts/deploy.js --network localhost
+
+npm run dev
+```
+
+# Notes
+
+- создание nest.js app
+```
+npx create-next-app@latest
+```
+
+- запуск next.js приложения: из папки front выполнить:
+```
+npm run dev
+```
+
+# Links
+
+https://www.youtube.com/watch?v=oicmTYQEKmU
---
+++ 15_frontend/scripts/deploy.js
@@ -0,0 +1,67 @@
+const hre = require("hardhat");
+const ethers = hre.ethers;
+const fs = require("fs");
+const path = require("path");
+const { network } = require("hardhat");
+
+async function main() {
+  if (network.name === "hardhat") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+      "gets automatically created and destroyed every time. Use the Hardhat" +
+      " option '--network localhost'"
+    )
+  }
+
+  const [deployer] = await ethers.getSigners()
+
+  console.log("Deploying with", await deployer.getAddress())
+
+  const DutchAuction = await ethers.getContractFactory("DutchAuction", deployer)
+  const auction = await DutchAuction.deploy(
+    ethers.utils.parseEther('2.0'),
+    1,
+    "Motorbike"
+  )
+  await auction.deployed()
+
+  saveFrontendFiles({
+    DutchAuction: auction
+  })
+}
+
+function saveFrontendFiles(contracts) {
+  const contractsDir = path.join(__dirname, '/..', 'front/contracts')
+
+  if(!fs.existsSync(contractsDir)) {
+    // добавлена опция {recursive: true} чтобы создать цепочку вложенных папок
+    fs.mkdirSync(contractsDir, {recursive: true})
+  }
+
+  Object.entries(contracts).forEach((contract_item) => {
+    const [name, contract] = contract_item
+
+    // файл с адресом контракта
+    if(contract) {
+      fs.writeFileSync(
+        path.join(contractsDir, '/', name + '-contract-address.json'),
+        JSON.stringify( {[name]: contract.address}, undefined, 2)
+      )
+    }
+
+    // файл с интерфейсом контракта
+    const ContractArtifact = hre.artifacts.readArtifactSync(name)
+
+    fs.writeFileSync(
+      path.join(contractsDir, '/', name + ".json"),
+      JSON.stringify(ContractArtifact, null, 2)
+    )
+  })
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
